45132ec0322e9bc02eb24f28a7ad4507ccc7cb3b
sebastien1998 <sebastien80001@gmail.com>
2021-11-19 17:03:20 +0100

    Adding a ribbon server module

---
+++ ribbonServer/src/test/java/com/example/m2dfs/ribbonServer/RibbonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m2dfs.ribbonServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
