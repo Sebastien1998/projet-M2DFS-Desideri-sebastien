8b0a720894f8ed52dcb43b3cdc74f877311a5928
sebastien1998 <sebastien80001@gmail.com>
2021-11-19 18:43:12 +0100

    Adding a RestController (but the dependencies are broken) Enabling Discovery Client

---
+++ ribbonServer/src/main/java/com/example/m2dfs/ribbonServer/RestController.java
@@ -0,0 +1,30 @@
+package com.example.m2dfs.ribbonServer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+
+@RestController
+public class RestController {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping("/")
+    public String health() {
+        return "I am Ok";
+    }
+
+    @GetMapping("/backend")
+    public String backend() {
+        System.out.println("Inside MyRestController::backend...");
+
+        String serverPort = environment.getProperty("local.server.port");
+
+        System.out.println("Port : " + serverPort);
+
+        return "Hello form Backend!!! " + " Host : localhost " + " :: Port : " + serverPort;
+    }
+}
