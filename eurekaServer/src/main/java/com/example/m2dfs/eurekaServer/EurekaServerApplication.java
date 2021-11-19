102bf3ffeb60ef9937df5e029f38e84dbf91e200
sebastien1998 <sebastien80001@gmail.com>
2021-11-19 13:24:37 +0100

    Fixing EurekaServer module pom

---
+++ eurekaServer/src/main/java/com/example/m2dfs/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.m2dfs.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
