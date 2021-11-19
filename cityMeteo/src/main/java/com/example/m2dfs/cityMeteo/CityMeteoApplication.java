c485898d9152659af9cd1a2923e28a3088ec76fb
sebastien1998 <sebastien80001@gmail.com>
2021-11-19 19:22:14 +0100

    Trying to add a circuit break

--- cityMeteo/src/main/java/com/example/m2dfs/cityMeteo/CityMeteoApplication.java
+++ cityMeteo/src/main/java/com/example/m2dfs/cityMeteo/CityMeteoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class CityMeteoApplication {
 
 	public static void main(String[] args) {
