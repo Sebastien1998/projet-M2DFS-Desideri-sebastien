67357a7b4774b7f644954b2721e28c5b69432a91
sebastien1998 <sebastien80001@gmail.com>
2021-11-19 14:45:19 +0100

    Fixing Eureka client dependencies

--- cityMeteo/src/main/java/com/example/m2dfs/cityMeteo/CityMeteoApplication.java
+++ cityMeteo/src/main/java/com/example/m2dfs/cityMeteo/CityMeteoApplication.java
@@ -2,6 +2,7 @@ package com.example.m2dfs.cityMeteo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
